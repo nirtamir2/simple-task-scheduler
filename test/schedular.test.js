37c8f6c43c4db51b65049ba3ddae97eb82e475be
Nir Tamir <nirtamir2@gmail.com>
2018-07-21 11:48:20 +0300

    add beforeEach and afterEach in doAt tests

--- test/schedular.test.js
+++ test/schedular.test.js
@@ -37,40 +37,40 @@ describe('schedular', () => {
   })
 
   describe('doAt', () => {
+    const now = new Date(2018, 7, 21, 11, 0, 0, 0)
+
+    let dateNowSpy
+    beforeEach(() => {
+      dateNowSpy = jest.spyOn(Date, 'now').mockImplementation(() => now)
+    })
+
+    afterEach(() => {
+      dateNowSpy.mockReset()
+      dateNowSpy.mockRestore()
+    })
+
     test('execute the function on a given time', () => {
       jest.useFakeTimers()
-
-      const today = new Date(2018, 7, 21, 11, 0, 0, 0)
       const tomorrow = new Date(2018, 7, 22, 11, 0, 0, 0)
       const DAY_TO_MILLISECONDS = 24 * 60 * 60 * 1000
-
-      const dateNowSpy = jest.spyOn(Date, 'now').mockImplementation(() => today)
 
       Schedular.doAt(fn, tomorrow)
       jest.advanceTimersByTime(DAY_TO_MILLISECONDS - 1)
       expect(fn).toHaveBeenCalledTimes(0)
       jest.advanceTimersByTime(1)
       expect(fn).toHaveBeenCalledTimes(1)
-
-      dateNowSpy.mockReset()
-      dateNowSpy.mockRestore()
     })
+
     test('throws error when invalid date', () => {
       expect(() => {
         Schedular.doAt(fn, {})
       }).toThrow()
     })
     test('throws error when past date', () => {
-      const today = new Date(2018, 7, 21, 11, 0, 0, 0)
       const yesterday = new Date(2018, 7, 20, 11, 0, 0, 0)
-      const dateNowSpy = jest.spyOn(Date, 'now').mockImplementation(() => today)
-
       expect(() => {
         Schedular.doAt(fn, yesterday)
       }).toThrow()
-
-      dateNowSpy.mockReset()
-      dateNowSpy.mockRestore()
     })
   })
 })
