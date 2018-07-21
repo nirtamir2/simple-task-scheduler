a100dc62148bb528d05938edcb9bb139afad2d60
Nir Tamir <nirtamir2@gmail.com>
2018-07-21 17:17:26 +0300

    remove useless readme files

--- src/README.js
+++
@@ -1,8 +0,0 @@
-const Scheduler = require('my-unwritten-schedule')
-
-const sendEmailtask = Scheduler.doAfter(() => {}, { days: 2 })
-const sendNotificationtask = Scheduler.doAt(() => {}, new Date())
-const resetCachetask = Scheduler.doRecurrent(() => {}, { days: 14 })
-Scheduler.cancel(task)
-Scheduler.reschedule(task, { days: 21 })
-Scheduler.stopAll()
