ec12eb9ecaca0640deda63d0b0e2128703b40a13
Nir Tamir <nirtamir2@gmail.com>
2018-07-21 11:44:11 +0300

    add method doAt in schedule

--- src/scheduler.js
+++ src/scheduler.js
@@ -4,5 +4,15 @@ const Timer = require('./timer')
 function doAfter(fn, time) {
   Timer.time(fn, toMilliseconds(time))
 }
+function doAt(fn, date) {
+  if (!(date instanceof Date)) {
+    throw new Error('date should be Date object')
+  }
+  const delay = date - Date.now()
+  if (delay < 0) {
+    throw new Error('date should be in the future')
+  }
+  Timer.time(fn, delay)
+}
 
-module.exports = { doAfter }
+module.exports = { doAfter, doAt }
--- test/schedular.test.js
+++ test/schedular.test.js
@@ -37,20 +37,40 @@ describe('schedular', () => {
   })
 
   describe('doAt', () => {
-    test.skip('execute the function on a given time', () => {
+    test('execute the function on a given time', () => {
       jest.useFakeTimers()
 
       const today = new Date(2018, 7, 21, 11, 0, 0, 0)
       const tomorrow = new Date(2018, 7, 22, 11, 0, 0, 0)
       const DAY_TO_MILLISECONDS = 24 * 60 * 60 * 1000
 
-      Date.now = jest.fn(() => today)
+      const dateNowSpy = jest.spyOn(Date, 'now').mockImplementation(() => today)
 
       Schedular.doAt(fn, tomorrow)
       jest.advanceTimersByTime(DAY_TO_MILLISECONDS - 1)
       expect(fn).toHaveBeenCalledTimes(0)
       jest.advanceTimersByTime(1)
       expect(fn).toHaveBeenCalledTimes(1)
+
+      dateNowSpy.mockReset()
+      dateNowSpy.mockRestore()
+    })
+    test('throws error when invalid date', () => {
+      expect(() => {
+        Schedular.doAt(fn, {})
+      }).toThrow()
+    })
+    test('throws error when past date', () => {
+      const today = new Date(2018, 7, 21, 11, 0, 0, 0)
+      const yesterday = new Date(2018, 7, 20, 11, 0, 0, 0)
+      const dateNowSpy = jest.spyOn(Date, 'now').mockImplementation(() => today)
+
+      expect(() => {
+        Schedular.doAt(fn, yesterday)
+      }).toThrow()
+
+      dateNowSpy.mockReset()
+      dateNowSpy.mockRestore()
     })
   })
 })
